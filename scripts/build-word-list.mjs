2f6aa35c50c50ab940eb0ecff50fc9f5b883acd0
mcirt <mcirt117@gmail.com>
2026-08-24 11:26:22 -0400

    Delete scripts directory

--- scripts/build-word-list.mjs
+++
@@ -1,5 +0,0 @@
-import { readFile, writeFile } from "node:fs/promises";
-const source=JSON.parse(await readFile(new URL("../node_modules/an-array-of-english-words/index.json",import.meta.url),"utf8"));
-const words=source.map((word)=>word.toUpperCase()).filter((word)=>word.length>=3&&word.length<=26&&/^[A-Z]+$/.test(word)).sort();
-await writeFile(new URL("../public/words.txt",import.meta.url),`${words.join("\n")}\n`);
-console.log(`Wrote ${words.length.toLocaleString()} words.`);
